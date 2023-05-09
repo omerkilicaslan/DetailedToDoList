634ea7e506ee9c6429a5568300d07f3fb9912a97
omerkilicaslan <omerrr_2001@hotmail.com>
2023-05-09 19:55:21 +0300

    Initial Commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Ömer Faruk Kılıçaslan on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
